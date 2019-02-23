a84e9fa648b7c4df6d56e22efe39f1af5b446ad0
kgudzius <kgudzius@gmail.com>
2019-02-23 17:05:15 +0200

    d

--- app/routes/realtime_routes.js
+++ app/routes/realtime_routes.js
@@ -157,6 +157,7 @@ module.exports = async function(app, db) {
 
       var date = new Date().toISOString().slice(0, 10);
       var staticVilnius = [];
+      console.log(transit.trips.length)
 
       transit.trips.forEach(trip => {
         //check if trip is opearating on the day
